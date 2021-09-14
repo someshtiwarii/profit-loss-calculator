bfc733dabc9acd6c357859cced59215262f423a3
someshtiwarii <someshtiwari96@gmail.com>
2021-09-14 08:52:42 +0530

    minor design changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var pPrice = document.querySelector(".input2");
 var qty = document.querySelector(".input3");
 var cPrice = document.querySelector(".input4");
 var outputDiv = document.querySelector(".output");
-var btn = document.querySelector(".submit-button");
+var btn = document.querySelector(".check-button");
 
 
 outputDiv.style.display = "none" ;
@@ -31,7 +31,7 @@ if( purchasePrice != 0 && quantity != 0 && currentPrice != 0){
    {
        const loss= (purchasePrice-currentPrice)*quantity;
        const lossPer = (((purchasePrice-currentPrice)*100)/purchasePrice);
-       outputDiv.innerText = ("Your " + stockName + " share is in loss of Rs. " + loss + "\n" + "Your loss percentage is: "+ lossPer + "%");
+       outputDiv.innerText = ("Your " + stockName + " share is in loss of Rs. " + loss + "\n" + "Your loss percentage is: "+ lossPer.toFixed(2) + "%");
        outputDiv.style.background = "#F87171";
 
        if(lossPer>50){
@@ -42,10 +42,9 @@ if( purchasePrice != 0 && quantity != 0 && currentPrice != 0){
    else{
        const profit = (currentPrice-purchasePrice)*quantity;
        const profitPer = (((currentPrice-purchasePrice)*100)/currentPrice);
-       outputDiv.innerText = ("Your " + stockName + " share is in profit of Rs. " + profit + "\n" + "Your profit percentage is: " + profitPer + "%");
+       outputDiv.innerText = ("Your " + stockName + " share is in profit of Rs. " + profit + "\n" + "Your profit percentage is: " + profitPer.toFixed(2) + "%");
        outputDiv.style.background = "#34D399";
    } 
-
 } 
 
 else{
